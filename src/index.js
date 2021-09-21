8edcfffde7d6f4e0f0e8b8bb19075c15e19b5a80
오일교 <qdaszx@naver.com>
2021-09-21 19:41:58 +0900

    게임종료 구현

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ startBtn.addEventListener("click", start);
 gameMessage.addEventListener("click", start);
 
 function start() {
+  player.inplay = true;
+  player.score = 0;
+  gameArea.innerHTML = "";
   gameMessage.classList.add("hide");
   startBtn.classList.add("hide");
   // 플레이어 만들기
+  let bird = document.createElement("div");
+  let wing = document.createElement("div");
   bird.setAttribute("class", "bird");
   wing.setAttribute("class", "wing");
   bird.appendChild(wing);
@@ -33,60 +38,78 @@ document.addEventListener("keyup", pressOff);
 let keys = {};
 
 function pressOn(e) {
-  console.log(e.code);
   keys[e.code] = true;
 }
 
 function pressOff(e) {
-  console.log(e.code);
   keys[e.code] = false;
 }
-
-// 플레이어 생성
-let bird = document.createElement("div");
-let wing = document.createElement("div");
 
 // 플레이어 정보를 담을 객체
 let player = {
   x: 0,
   y: 0,
   speed: 2,
-  score: 0
+  score: 0,
+  inplay: false
 };
 
 // 애니메이션 구현 함수
 function playGame() {
-  // 날개짓 불린값
-  let move = true;
-  // 이동시 해당 x, y 계산
-  if (keys.ArrowLeft && player.x > 0) {
-    player.x -= player.speed;
-    move = true;
-  }
-  if (keys.ArrowRight && player.x < gameArea.offsetWidth - bird.offsetWidth) {
-    player.x += player.speed;
-    move = true;
-  }
-  if ((keys.ArrowUp || keys.Space) && player.y > 0) {
-    player.y -= player.speed * 5;
-    move = true;
-  }
-  if (keys.ArrowDown && player.y < gameArea.offsetHeight - bird.offsetHeight) {
-    player.y += player.speed;
-    move = true;
-  }
+  // 인플레이 true (게임 진행 중)
+  if (player.inplay) {
+    let bird = document.querySelector(".bird");
+    let wing = document.querySelector(".wing");
+    // 날개짓 불린값
+    let move = true;
+    // 이동시 해당 x, y 계산
+    if (keys.ArrowLeft && player.x > 0) {
+      player.x -= player.speed;
+      move = true;
+    }
+    if (keys.ArrowRight && player.x < gameArea.offsetWidth - bird.offsetWidth) {
+      player.x += player.speed;
+      move = true;
+    }
+    if ((keys.ArrowUp || keys.Space) && player.y > 0) {
+      player.y -= player.speed * 5;
+      move = true;
+    }
+    if (
+      keys.ArrowDown &&
+      player.y < gameArea.offsetHeight - bird.offsetHeight
+    ) {
+      player.y += player.speed;
+      move = true;
+    }
 
-  if (move) {
-    wing.pos = wing.pos === 15 ? 25 : 15;
-    wing.style.top = wing.pos + "px";
+    // 날개 움직임
+    if (move) {
+      wing.pos = wing.pos === 15 ? 25 : 15;
+      wing.style.top = wing.pos + "px";
+    }
+
+    player.y += player.speed * 2;
+    // 게임 종료
+    if (player.y > gameArea.offsetHeight) {
+      playGameOver();
+    }
+
+    bird.style.left = player.x + "px";
+    bird.style.top = player.y + "px";
+    window.requestAnimationFrame(playGame);
+    // 스코어
+    player.score++;
+    score.innerText = "SCORE : " + player.score;
   }
+}
 
-  player.y += player.speed * 2;
-
-  bird.style.left = player.x + "px";
-  bird.style.top = player.y + "px";
-  window.requestAnimationFrame(playGame);
-  // 스코어
-  player.score++;
-  score.innerText = "SCORE : " + player.score;
+// 게임 종료 함수
+function playGameOver() {
+  player.inplay = false;
+  gameMessage.classList.remove("hide");
+  gameMessage.innerHTML =
+    "GAME OVER<br/>당신의 점수는 " +
+    player.score +
+    "점 입니다. <br/> 다시 시작하려면 여기를 누르세요";
 }
