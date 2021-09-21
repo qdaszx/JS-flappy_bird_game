30050c44542bc695c345d46c66b3ca62951f2754
오일교 <qdaszx@naver.com>
2021-09-21 19:17:22 +0900

    중력 만들기 중력 => player.y += player.speed * 2;
    중력보다 큰 값
    or 연산자 + keys.Space
    speed * 5

--- src/index.js
+++ src/index.js
@@ -66,8 +66,8 @@ function playGame() {
     player.x += player.speed;
     move = true;
   }
-  if (keys.ArrowUp && player.y > 0) {
-    player.y -= player.speed;
+  if ((keys.ArrowUp || keys.Space) && player.y > 0) {
+    player.y -= player.speed * 5;
     move = true;
   }
   if (keys.ArrowDown && player.y < gameArea.offsetHeight - bird.offsetHeight) {
@@ -79,6 +79,8 @@ function playGame() {
     wing.pos = wing.pos === 15 ? 25 : 15;
     wing.style.top = wing.pos + "px";
   }
+
+  player.y += player.speed * 2;
 
   bird.style.left = player.x + "px";
   bird.style.top = player.y + "px";
