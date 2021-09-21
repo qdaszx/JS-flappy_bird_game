c4bdec0f73e788d7d68f27fc0e6583c193b5354f
오일교 <qdaszx@naver.com>
2021-09-21 14:25:49 +0900

    requestAnimationFrame 적용 방향키마다 x,y축 적용

--- src/index.js
+++ src/index.js
@@ -12,10 +12,15 @@ gameMessage.addEventListener("click", start);
 function start() {
   gameMessage.classList.add("hide");
   startBtn.classList.add("hide");
+  // 플레이어 만들기
   bird.setAttribute("class", "bird");
   wing.setAttribute("class", "wing");
   bird.appendChild(wing);
   gameArea.appendChild(bird);
+  // 움직임 처리 함수
+  player.x = bird.offsetLeft;
+  player.y = bird.offsetTop;
+  window.requestAnimationFrame(playGame);
 }
 
 // 방향키 감지 함수
@@ -37,3 +42,29 @@ function pressOff(e) {
 // 플레이어 생성
 let bird = document.createElement("div");
 let wing = document.createElement("div");
+
+// 플레이어 정보를 담을 객체
+let player = {
+  x: 0,
+  y: 0,
+  speed: 2
+};
+
+// 애니메이션 구현 함수
+function playGame() {
+  if (keys.ArrowLeft) {
+    player.x -= player.speed;
+  }
+  if (keys.ArrowRight) {
+    player.x += player.speed;
+  }
+  if (keys.ArrowUp) {
+    player.y -= player.speed;
+  }
+  if (keys.ArrowDown) {
+    player.y += player.speed;
+  }
+  bird.style.left = player.x + "px";
+  bird.style.top = player.y + "px";
+  window.requestAnimationFrame(playGame);
+}
